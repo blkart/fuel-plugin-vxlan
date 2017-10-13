b389dc76ee8b7ee19b8d63340625b338d6f3bb08
blkart <blkart.org@gmail.com>
2017-10-13 16:46:47 +0800

    optimization for hyper-converged deployment
    There's nothing to do for compute role deployment when compute and
    controller role in the same node.
    
    Signed-off-by: blkart <blkart.org@gmail.com>

--- deployment_scripts/puppet/manifests/site-compute-post.pp
+++ deployment_scripts/puppet/manifests/site-compute-post.pp
@@ -1,2 +1,9 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
-class {'vxlan::compute':}
+$roles         = node_roles($::fuel_settings['nodes'], $::fuel_settings['uid'])
+# for hyper-converged: there's nothing to do for vxlan
+# when compute and controller role in the same node.
+if (! member($roles, 'controller') and ! member($roles, 'primary-controller')){
+  class {'vxlan::compute':}
+} else {
+  notice('Compute role with primary-controller/controller role in the same node, nothing to do.')
+}
